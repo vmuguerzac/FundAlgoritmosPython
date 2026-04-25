42916b67b24b0a5d86c5a5df7b43bca85f714c04
vmuguerzac <vmuguerzac@gmail.com>
2026-04-24 20:42:04 -0500

    upd: se ha agregado la funcion Area de Triangulo

--- s5_funciones.py
+++ s5_funciones.py
@@ -7,10 +7,26 @@ def AreaCirculo(radio):
     # retornar el resultado al usuario
     return resultado
 
+def AreaTriangulo(base, altura):
+    resultado = (base * altura) / 2
+    return resultado
+
 # Obtener el valor del radio del usuario
 radio = float(input("Ingrese radio del circulo: "))
 # invocar a la funcion para calcular el valor de area del circulo
 resultado = AreaCirculo(radio)
 print("El valor de area del circulo es: ", resultado)
+# Obtener el area de un triangulo
+# obtener los valores de entrada del usuario (inputs)
+base = float(input("Ingrese base del triangulo: "))
+altura = float(input("Ingrese altura del triangulo: "))
+# validar inputs
+if base > 0 and altura > 0:
+    # procesar los inputs para obtener la salida (output)
+    resultado = AreaTriangulo(base, altura)
+    # mostrar al usuario la salida (output)
+    print("El area del triangulo es: ", resultado)
+else:
+    print("Existe un error en los valores de entrada")
 print("Fin del programa")
 
