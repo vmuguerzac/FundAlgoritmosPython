72b4234764c948086625c9a21044ab376663c9d6
vmuguerzac <vmuguerzac@gmail.com>
2026-04-27 21:37:33 -0500

    Agregado S6 ejemplo imc con funciones

---
+++ s6_calcular_imc_v3.py
@@ -0,0 +1,32 @@
+def CalcularIMC(peso, altura):
+    # validar datos de entrada
+    valido = ValidarEntrada(peso, altura)
+    if valido  == True:
+        # Proceso
+        imc = peso / altura ** 2
+    else:
+        print("Datos de entrada no valido")
+    return imc
+
+def ValidarEntrada(peso, altura):
+    valido = False
+    if peso <= 400 and peso >= 5:
+        if altura <= 2.5 and altura >= 0.6:
+          valido = True
+        else:
+            print("Altura inválida")
+            valido = False
+    else:
+        print("Peso invalido")
+        valido = False
+    return valido  
+
+# Programa que calcula el IMC
+# Declaracion y obtencion de datos
+peso = float(input("Ingrese su peso (Kg): "))
+altura = float(input("Ingrese su altura (m): "))
+imc = CalcularIMC(peso, altura)
+if imc > 0:
+    # Mostrar informacion
+    print("Su imc es: ", imc)
+
